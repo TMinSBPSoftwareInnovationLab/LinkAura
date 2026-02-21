3529877eaf3f82c97e8256939c6e67142bd6839a
Lakshmipathy Balakarthi <slbm.balakarthi@gmail.com>
2026-02-21 11:48:52 +0530

    21-02-2026 filesystems reset

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,109 +60,16 @@ return [
         //     'report' => false,
         // ],
 
-        'disks' => [
-
-            'local' => [
-                'driver' => 'local',
-                'root' => storage_path('app/private'),
-                'serve' => true,
-                'throw' => false,
-                'report' => false,
-            ],
-
-            'public' => [
-                'driver' => 'local',
-                'root' => storage_path('app/public'),
-                'url' => env('APP_URL').'/storage',
-                'visibility' => 'public',
-                'throw' => false,
-                'report' => false,
-            ],
-
-            's3_billing' => [
-                'driver' => 's3',
-                'key' => env('AWS_ACCESS_KEY_ID'),
-                'secret' => env('AWS_SECRET_ACCESS_KEY'),
-                'region' => env('AWS_DEFAULT_REGION'),
-                'bucket' => env('AWS_BUCKET_BILLING'),
-                'url' => env('AWS_URL_BILLING'),
-                'endpoint' => env('AWS_ENDPOINT'),
-                'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-                'visibility' => 'public',
-            ],
-
-            // NEW Gallery Disk
-            's3_gallery' => [
-                'driver' => 's3',
-                'key' => env('AWS_ACCESS_KEY_ID'),
-                'secret' => env('AWS_SECRET_ACCESS_KEY'),
-                'region' => env('AWS_DEFAULT_REGION'),
-                'bucket' => env('AWS_BUCKET_GALLERY'),
-                'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-                'url' => env('AWS_URL_GALLERY'),
-                'visibility' => 'public', // Neenga public access kuduthu irukkarathal
-            ],
-
-            // NEW COMPANY LOGO Disk
-            's3_company_logos' => [
-                'driver' => 's3',
-                'key' => env('AWS_ACCESS_KEY_ID'),
-                'secret' => env('AWS_SECRET_ACCESS_KEY'),
-                'region' => env('AWS_DEFAULT_REGION'),
-                'bucket' => env('AWS_BUCKET_LOGO'),
-                'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-                'url' => env('AWS_URL_LOGO'),
-                'visibility' => 'public', // Neenga public access kuduthu irukkarathal
-            ],
-
-            // NEW PRODUCT IMAGES Disk
-            's3_products' => [
-                'driver' => 's3',
-                'key' => env('AWS_ACCESS_KEY_ID'),
-                'secret' => env('AWS_SECRET_ACCESS_KEY'),
-                'region' => env('AWS_DEFAULT_REGION'),
-                'bucket' => env('AWS_BUCKET_PRODUCT_IMAGES'),
-                'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-                'url' => env('AWS_URL_PRODUCT_IMAGES'),
-                'visibility' => 'public', // Neenga public access kuduthu irukkarathal
-            ],
-
-            // NEW SERVICE IMAGES Disk
-            's3_services' => [
-                'driver' => 's3',
-                'key' => env('AWS_ACCESS_KEY_ID'),
-                'secret' => env('AWS_SECRET_ACCESS_KEY'),
-                'region' => env('AWS_DEFAULT_REGION'),
-                'bucket' => env('AWS_BUCKET_SERVICE_IMAGES'),
-                'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-                'url' => env('AWS_URL_SERVICE_IMAGES'),
-                'visibility' => 'public', // Neenga public access kuduthu irukkarathal
-            ],
-
-
-            // NEW PAYMENT DETAILS Disk
-            's3_payments' => [
-                'driver' => 's3',
-                'key' => env('AWS_ACCESS_KEY_ID'),
-                'secret' => env('AWS_SECRET_ACCESS_KEY'),
-                'region' => env('AWS_DEFAULT_REGION'),
-                'bucket' => env('AWS_BUCKET_PAYMENT_DETAILS'),
-                'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-                'url' => env('AWS_URL_PAYMENT_DETAILS'),
-                'visibility' => 'public', // Neenga public access kuduthu irukkarathal
-            ],
-
-            // NEW QRCODE Disk
-            's3_qr' => [
-                'driver' => 's3',
-                'key' => env('AWS_ACCESS_KEY_ID'),
-                'secret' => env('AWS_SECRET_ACCESS_KEY'),
-                'region' => env('AWS_DEFAULT_REGION'),
-                'bucket' => env('AWS_BUCKET_QRCODE'),
-                'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-                'url' => env('AWS_URL_QRCODE'),
-                'visibility' => 'public', // Neenga public access kuduthu irukkarathal
-            ],
+        's3_billing' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET_BILLING'),
+            'url' => env('AWS_URL_BILLING'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => true,
+            'visibility' => 'public',
         ],
 
         // Puthu Gallery Disk
