e7fe6f92f1f2b1155e6c1ac499975027d5959c5b
Lakshmipathy Balakarthi <slbm.balakarthi@gmail.com>
2026-02-27 19:39:18 +0530

    27-02-2026 linkaura footer redesign done

--- resources/js/Pages/Mini_Website_Pages/common_footer/linkaura_foot.vue
+++ resources/js/Pages/Mini_Website_Pages/common_footer/linkaura_foot.vue
@@ -1,8 +1,21 @@
 <template>
-    <div class="max-w-[430px] p-2 bg-[#000b58] mt-1">
-        <p class="text-gray-100 font-bold text-[12px] text-center">
-            © {{ new Date().getFullYear() }} LinkAura, a Digital Mini-Website & Smart Card Platform. All Rights Reserved.
+    <div class="max-w-[430px] mx-auto mt-3 bg-gradient-to-r from-[#000b58] via-[#001a8c] to-[#000b58] ounded-xl shadow-lg p-4 text-center">
+        <p class="text-gray-200 text-xs font-medium leading-relaxed">
+            © {{ new Date().getFullYear() }} 
+            <span class="font-semibold text-white">LinkAura</span>,  
+            a Digital Mini-Website & Smart Card Platform.  
+            All Rights Reserved.
         </p>
+
+        <div class="my-3 border-t border-white/30"></div>
+
+        <a href="https://linkaura.co.in/" 
+        target="_blank"
+        class="inline-block text-sm font-semibold text-white 
+                hover:text-yellow-300 transition duration-300">
+            Visit LinkAura Website →
+        </a>
+
     </div>
 </template>
 
