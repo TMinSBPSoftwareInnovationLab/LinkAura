aefffc5a1aae9787343456facad95bea5190dcd8
Lakshmipathy Balakarthi <slbm.balakarthi@gmail.com>
2026-03-08 11:11:14 +0530

    08-03-2026 Sidebar user edit button Hide

--- resources/js/Pages/Components/SideNavBar.vue
+++ resources/js/Pages/Components/SideNavBar.vue
@@ -42,6 +42,7 @@
                          @click="goToProfileEdit" 
                         class="text-blue-600 hover:text-blue-800 transition"
                         title="Edit Profile"
+                        v-if="1==2"
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="gray">
                         <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
