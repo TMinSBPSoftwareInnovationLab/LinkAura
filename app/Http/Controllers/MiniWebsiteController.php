f402ef3069e560267e62d7c8f3c53c8ebfc4e8f7
Lakshmipathy Balakarthi <slbm.balakarthi@gmail.com>
2026-04-12 13:35:39 +0530

    12-04-2026 Temp 1 Purchase area qty new field added

--- app/Http/Controllers/MiniWebsiteController.php
+++ app/Http/Controllers/MiniWebsiteController.php
@@ -2423,7 +2423,7 @@ class MiniWebsiteController extends Controller
         DB::table('miniweb_orders')->insert([
             'company_id' => $product->mini_website_id,
             'product_id' => $validated['product_id'],
-            'productQty' => $validated['productQty'],
+            'qty' => $validated['productQty'],
             'product_name' => $validated['product_name'],
             'product_price' => $validated['product_price'],
             'customer_name' => $validated['customer_name'],
