e37214174b25de2c0896a8f74a8a79c3f432f3c3
Lakshmipathy Balakarthi <slbm.balakarthi@gmail.com>
2026-04-13 19:46:20 +0530

    13-04-2026 home page logo og meta image update

--- routes/web.php
+++ routes/web.php
@@ -269,7 +269,7 @@ Route::get('/{company}/Website_Temp_{themeIdEnc}', function ($company, $themeIdE
 */
 Route::get('/', function () {
 
-    $linauraLogo = "https://linkaura.co.in/build/assets/linkAura_logo-Dcby3HVP.png";
+    $linauraLogo = asset("images/linkAuraLogo300.png");
 
     return Inertia::render('Website/Index', [
         'metaTitle' => 'LinkAura - Build Your Digital Identity',
