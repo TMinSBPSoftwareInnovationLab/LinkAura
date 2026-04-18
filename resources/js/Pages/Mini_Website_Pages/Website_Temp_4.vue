76536ba3706d170ff8d290c8e5a7430ffcb5bc43
Lakshmipathy Balakarthi <slbm.balakarthi@gmail.com>
2026-04-18 18:49:39 +0530

    18-06-2026 Temp-2-6 product order total price updated

--- resources/js/Pages/Mini_Website_Pages/Website_Temp_4.vue
+++ resources/js/Pages/Mini_Website_Pages/Website_Temp_4.vue
@@ -1969,6 +1969,7 @@
                         product_name: proName,
                         product_price: price,
                         productQty: values.productQty,
+                        totalPrice: totalPrice,
                         customer_name: values.customerName,
                         customer_phone: values.customerPhone,
                         customer_address: values.customerAddress
